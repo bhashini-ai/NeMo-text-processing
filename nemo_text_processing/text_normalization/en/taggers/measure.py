fa4d00fd6f4bef0fe74b137daef8aede81151dcf
pranav0963 <94529251+pranav0963@users.noreply.github.com>
2025-01-23 10:14:25 +0530

    by pranav

--- nemo_text_processing/text_normalization/en/taggers/measure.py
+++ nemo_text_processing/text_normalization/en/taggers/measure.py
@@ -141,6 +141,7 @@ class MeasureFst(GraphFst):
             pynutil.insert("decimal { ")
             + decimal.final_graph_wo_negative
             + pynini.cross("-", "")
+            + pynini.cross("\'s", "s")
             + pynutil.insert(' } units: "')
             + pynini.closure(NEMO_ALPHA, 1)
             + pynutil.insert('"')
